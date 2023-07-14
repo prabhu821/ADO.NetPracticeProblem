66a67781233336ca5e2b0e43162954bef57b4cb1
rishiraj007 <rishiraj@mounttalent.com>
2023-07-14 17:02:26 +0530

    [Prabhu]Add: ADO.Net Practice Problem

---
+++ ADO.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ADO.Net Practice Problem!");
+        }
+    }
+}
